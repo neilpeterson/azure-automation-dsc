93ee4317cdb280613a6267f1e0f22710e302dd94
Neil Peterson <neilpeterson@outlook.com>
2018-10-24 21:26:37 +0200

    Adding AA variable

--- support-scripts/alert-to-teams-runbook.ps1
+++ support-scripts/alert-to-teams-runbook.ps1
@@ -7,7 +7,7 @@
     Intent: Sample to demonstrate Azure Automation Features.
  #>
 
-Param(
+ Param(
     [parameter (Mandatory=$false)]
     [object] $WebhookData
 )
@@ -16,25 +16,18 @@ $RequestBody = $WebhookData.RequestBody | ConvertFrom-Json
 
 $Data = $RequestBody.data
 
-# Teams Webhook URI
+Write-Output $RequestBody
+Write-Output "-------"
+Write-Output $Data.name
+
+# # Teams Webhook URI
 $TeamsURI = Get-AutomationVariable -Name 'TeamsURI'
 
-If($Data.operationName -match "Microsoft.ContainerInstance/containerGroups/write" -and $Data.status -match "Succeeded") {
 
-    # Runbook Ouput
-    Write-Output "VM Alert.."
-    Write-Output $Data.resourceUri
 
-    # Get Contianer Instance Name
-    $name = ($Data.resourceUri.Split("/"))[-1]
-
-    # Notify Team
-    $Body = ConvertTo-Json @{
-        text = 'Contianer Instance Created: ' + $name
-    }
-    Invoke-WebRequest -Uri $TeamsURI -Method Post -Body $Body -ContentType 'application/json' -UseBasicParsing
+# Notify Team
+$Body = ConvertTo-Json @{
+    text = 'Azure Monitor Alert: ' + $Data.name
 }
-else
-{
-    Write-Error "Could not find Container Instance write event"
-}
+
+Invoke-WebRequest -Uri $TeamsURI -Method Post -Body $Body -ContentType 'application/json' -UseBasicParsing
--- support-scripts/run-ps-script-vm.ps1
+++ support-scripts/run-ps-script-vm.ps1
@@ -7,21 +7,13 @@
     Intent: Sample to demonstrate Azure VM Custom Script Extension.
  #>
 
- Param(
-    [parameter (Mandatory=$true)]
-    [string] $vmName
-)
-
- $cred = Get-AutomationPSCredential -Name azureCredentials
- Add-AzureAccount -Credential $cred
-
  $params = @{
-    resourceGroupName = "win-server";
-    location = "eastus";
-    name = "win-server";
-    vmName = "win-server";
-    FileUri = "https://raw.githubusercontent.com/neilpeterson/azure-powershell-demos/master/2-custom-script-extension/log-script.ps1";
-    Run = "log-script.ps1"
+    resourceGroupName = "lp4s3demo";
+    location = "westeurope";
+    name = "win-vm-0";
+    vmName = "win-vm-0";
+    FileUri = "https://raw.githubusercontent.com/neilpeterson/azure-automation-dsc/master/support-scripts/cpu-leak.ps1";
+    Run = "cpu-leak.ps1"
 }
 
 Set-AzVMCustomScriptExtension @params
